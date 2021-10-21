a450d00b5cbae3968d1aed72f8376bdd45423357
Aman50 <amanbhalla50@gmail.com>
2021-10-21 23:50:47 +0530

    Added custom attribute directive

---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,28 @@
+import { Directive, ElementRef, HostBinding, HostListener } from "@angular/core";
+
+@Directive({
+    selector: '[appDropdown]'
+})
+export class DropdownDirective {
+
+    @HostBinding('class.open')
+    isOpen: boolean = false;
+
+    // @HostListener('click') onClickToggle() {
+    //     this.isOpen = !this.isOpen;
+    // }
+
+    @HostListener('document:click', ['$event']) onClick(event: Event) {
+        console.log(this.elementRef.nativeElement);
+        if (this.elementRef.nativeElement.contains(event.target)){
+            this.isOpen = !this.isOpen;
+        } else {
+            this.isOpen = false;
+        }
+
+
+
+    }
+
+    constructor(private elementRef: ElementRef){}
+}
